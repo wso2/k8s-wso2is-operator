45a5240fd5d3b83667f38d3ec09a5a307c9933e1
Suresh Peiris <tsmpmail@gmail.com>
2021-01-21 18:35:36 +0530

    Added default values to CRD and simplified

--- api/v1beta1/wso2is_types.go
+++ api/v1beta1/wso2is_types.go
@@ -31,72 +31,80 @@ type Wso2IsSpec struct {
 }
 
 type Configurations struct {
-	Host       string     `json:"host"`
-	Server     Server     `json:"server" toml:"server"`
+	Host string `json:"host"`
+	// +kubebuilder:default:={ "hostname" : "$env{HOST_NAME}", "nodeIp": "$env{NODE_IP}" }
+	Server     Server     `json:"server,omitempty" toml:"server"`
 	SuperAdmin SuperAdmin `json:"superAdmin" toml:"super_admin"`
 	UserStore  UserStore  `json:"userStore" toml:"user_store"`
 	Database   Database   `json:"database" toml:"database"`
 	Transport  Transport  `json:"transport" toml:"transport"`
-	//Datasource     []Datasource   `json:"datasource" toml:"datasource"`
-	//Authentication Authentication `json:"authentication" toml:"authentication"`
 	Keystore   Keystore   `json:"keystore" toml:"keystore"`
 	Clustering Clustering `json:"clustering" toml:"clustering"`
-	Monitoring Monitoring `json:"monitoring" toml:"monitoring"`
-	Hazelcast  Hazelcast  `json:"hazelcast" toml:"hazelcast"`
+	// +kubebuilder:default:={ "jmx" : { "rmi_server_start" : true } }
+	Monitoring Monitoring `json:"monitoring,omitempty" toml:"monitoring"`
+	// +kubebuilder:default:={ "hazelcastShutdownhookEnabled" : false, "hazelcastLoggingType" : "log4j"  }
+	Hazelcast Hazelcast `json:"hazelcast,omitempty" toml:"hazelcast"`
 }
 type Server struct {
-	Hostname string `json:"hostname" toml:"hostname"`
-	NodeIP   string `json:"nodeIp" toml:"node_ip"`
-	//BasePath string `json:"basePath" toml:"base_path"`
+	// +kubebuilder:default:="$env{HOST_NAME}"
+	Hostname string `json:"hostname,omitempty" toml:"hostname"`
+	// +kubebuilder:default:="$env{NODE_IP}"
+	NodeIP string `json:"nodeIp,omitempty" toml:"node_ip"`
 }
 type SuperAdmin struct {
-	Username           string `json:"username" toml:"username"`
-	Password           string `json:"password" toml:"password"`
-	CreateAdminAccount bool   `json:"createAdminAccount" toml:"create_admin_account"`
+	Username string `json:"username" toml:"username"`
+	Password string `json:"password" toml:"password"`
+	// +kubebuilder:default:=true
+	CreateAdminAccount bool `json:"createAdminAccount,omitempty" toml:"create_admin_account"`
 }
 type UserStore struct {
 	Type string `json:"type" toml:"type"`
 }
 type Hazelcast struct {
-	ShutdownHookEnabled bool   `json:"hazelcastShutdownhookEnabled" toml:"hazelcast.shutdownhook.enabled"`
-	LoggingType         string `json:"hazelcastLoggingType" toml:"hazelcast.logging.type"`
+	// +kubebuilder:default:=false
+	ShutdownHookEnabled bool `json:"hazelcastShutdownhookEnabled,omitempty" toml:"hazelcast.shutdownhook.enabled"`
+	// +kubebuilder:default:="log4j"
+	LoggingType string `json:"hazelcastLoggingType,omitempty" toml:"hazelcast.logging.type"`
 }
 type PoolOptions struct {
-	ValidationQuery string `json:"validationQuery" toml:"validationQuery"`
+	// +kubebuilder:default:="SELECT 1"
+	ValidationQuery string `json:"validationQuery,omitempty" toml:"validationQuery"`
 }
 type User struct {
-	URL         string      `json:"url" toml:"url"`
-	Username    string      `json:"username" toml:"username"`
-	Password    string      `json:"password" toml:"password"`
-	Driver      string      `json:"driver" toml:"driver"`
-	PoolOptions PoolOptions `json:"pool_options" toml:"pool_options"`
+	URL      string `json:"url" toml:"url"`
+	Username string `json:"username" toml:"username"`
+	Password string `json:"password" toml:"password"`
+	Driver   string `json:"driver" toml:"driver"`
+	// +kubebuilder:default:={ "validationQuery" : "SELECT 1" }
+	PoolOptions PoolOptions `json:"pool_options,omitempty" toml:"pool_options"`
 }
 type IdentityDb struct {
-	URL         string      `json:"url" toml:"url"`
-	Username    string      `json:"username" toml:"username"`
-	Password    string      `json:"password" toml:"password"`
-	Driver      string      `json:"driver" toml:"driver"`
-	PoolOptions PoolOptions `json:"pool_options"`
+	URL      string `json:"url" toml:"url"`
+	Username string `json:"username" toml:"username"`
+	Password string `json:"password" toml:"password"`
+	Driver   string `json:"driver" toml:"driver"`
+	// +kubebuilder:default:={ "validationQuery" : "SELECT 1" }
+	PoolOptions PoolOptions `json:"pool_options,omitempty" toml:"pool_options"`
 }
 type SharedDb struct {
-	URL         string      `json:"url" toml:"url"`
-	Username    string      `json:"username" toml:"username"`
-	Password    string      `json:"password" toml:"password"`
-	Driver      string      `json:"driver" toml:"driver"`
-	PoolOptions PoolOptions `json:"pool_options" toml:"pool_options"`
+	URL      string `json:"url" toml:"url"`
+	Username string `json:"username" toml:"username"`
+	Password string `json:"password" toml:"password"`
+	Driver   string `json:"driver" toml:"driver"`
+	// +kubebuilder:default:={ "validationQuery" : "SELECT 1" }
+	PoolOptions PoolOptions `json:"pool_options,omitempty" toml:"pool_options"`
 }
 type BpsDatabase struct {
-	URL         string      `json:"url" toml:"url"`
-	Username    string      `json:"username" toml:"username"`
-	Password    string      `json:"password" toml:"password"`
-	Driver      string      `json:"driver" toml:"driver"`
-	PoolOptions PoolOptions `json:"pool_options" toml:"pool_options"`
+	URL      string `json:"url" toml:"url"`
+	Username string `json:"username" toml:"username"`
+	Password string `json:"password" toml:"password"`
+	Driver   string `json:"driver" toml:"driver"`
+	// +kubebuilder:default:={ "validationQuery" : "SELECT 1" }
+	PoolOptions PoolOptions `json:"pool_options,omitempty" toml:"pool_options"`
 }
 type Database struct {
-	//User        User        `json:"user" toml:"user"`
 	IdentityDb IdentityDb `json:"identityDb" toml:"identity_db"`
 	SharedDb   SharedDb   `json:"sharedDb" toml:"shared_db"`
-	//BpsDatabase BpsDatabase `json:"bpsDb" toml:"bps_database"`
 }
 type Properties struct {
 	ProxyPort int `json:"proxyPort" toml:"proxyPort"`
@@ -133,23 +141,31 @@ type Keystore struct {
 	Primary Primary `json:"primary" toml:"primary"`
 }
 type Clustering struct {
-	MembershipScheme string               `json:"membership_scheme" toml:"membership_scheme"`
-	Domain           string               `json:"domain" toml:"domain"`
-	Properties       ClusteringProperties `json:"properties" toml:"properties"`
+	// +kubebuilder:default:="kubernetes"
+	MembershipScheme string `json:"membership_scheme,omitempty" toml:"membership_scheme"`
+	// +kubebuilder:default:="wso2.is.domain"
+	Domain     string               `json:"domain,omitempty" toml:"domain"`
+	Properties ClusteringProperties `json:"properties" toml:"properties"`
 }
 type ClusteringProperties struct {
-	PropertiesMembershipSchemeClassName           string `json:"membershipSchemeClassName" toml:"membershipSchemeClassName"`
-	PropertiesKUBERNETESNAMESPACE                 string `json:"KUBERNETES_NAMESPACE" toml:"KUBERNETES_NAMESPACE"`
-	PropertiesKUBERNETESSERVICES                  string `json:"KUBERNETES_SERVICES" toml:"KUBERNETES_SERVICES"`
-	PropertiesKUBERNETESMASTERSKIPSSLVERIFICATION bool   `json:"KUBERNETES_MASTER_SKIP_SSL_VERIFICATION" toml:"KUBERNETES_MASTER_SKIP_SSL_VERIFICATION"`
-	PropertiesUSEDNS                              bool   `json:"USE_DNS" toml:"USE_DNS"`
-	PropertiesKUBERNETES_API_SERVER               string `json:"KUBERNETES_API_SERVER" toml:"KUBERNETES_API_SERVER"`
+	// +kubebuilder:default:="org.wso2.carbon.membership.scheme.kubernetes.KubernetesMembershipScheme"
+	PropertiesMembershipSchemeClassName string `json:"membershipSchemeClassName,omitempty" toml:"membershipSchemeClassName"`
+	// +kubebuilder:default:="default"
+	PropertiesKUBERNETESNAMESPACE string `json:"KUBERNETES_NAMESPACE,omitempty" toml:"KUBERNETES_NAMESPACE"`
+	// +kubebuilder:default:="wso2is-service"
+	PropertiesKUBERNETESSERVICES string `json:"KUBERNETES_SERVICES,omitempty" toml:"KUBERNETES_SERVICES"`
+	// +kubebuilder:default:=true
+	PropertiesKUBERNETESMASTERSKIPSSLVERIFICATION bool `json:"KUBERNETES_MASTER_SKIP_SSL_VERIFICATION,omitempty" toml:"KUBERNETES_MASTER_SKIP_SSL_VERIFICATION"`
+	// +kubebuilder:default:=false
+	PropertiesUSEDNS                bool   `json:"USE_DNS,omitempty" toml:"USE_DNS,omitempty"`
+	PropertiesKUBERNETES_API_SERVER string `json:"KUBERNETES_API_SERVER" toml:"KUBERNETES_API_SERVER"`
 }
 type Jmx struct {
-	RmiServerStart bool `toml:"rmi_server_start" json:"rmi_server_start"`
+	// +kubebuilder:default:=true
+	RmiServerStart bool `toml:"rmi_server_start" json:"rmi_server_start,omitempty"`
 }
 type Monitoring struct {
-	Jmx Jmx `toml:"jmx" json:"jmx"`
+	Jmx Jmx `toml:"jmx" json:"jmx,omitempty"`
 }
 
 // Wso2IsStatus defines the observed state of Wso2Is
