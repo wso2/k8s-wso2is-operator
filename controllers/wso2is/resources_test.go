58d75bb26516e609090b2262f432837d2360eedf
RukshanJS <rjsenanayaka@gmail.com>
2023-07-26 23:08:42 +0530

    chore: resources unit test bug fix

--- controllers/wso2is/resources_test.go
+++ controllers/wso2is/resources_test.go
@@ -14,15 +14,15 @@ var _ = Describe("ResourceRequirements", func() {
 
 		// Verify Requests
 		expectedRequests := corev1.ResourceList{
-			corev1.ResourceCPU:    resource.MustParse("4Gi"),
-			corev1.ResourceMemory: resource.MustParse("4096m"),
+			corev1.ResourceCPU:    resource.MustParse("1"),
+			corev1.ResourceMemory: resource.MustParse("4096Mi"),
 		}
 		Expect(resourceReqs.Requests).To(Equal(expectedRequests))
 
 		// Verify Limits
 		expectedLimits := corev1.ResourceList{
-			corev1.ResourceCPU:    resource.MustParse("4Gi"),
-			corev1.ResourceMemory: resource.MustParse("4096m"),
+			corev1.ResourceCPU:    resource.MustParse("2"),
+			corev1.ResourceMemory: resource.MustParse("8000Mi"),
 		}
 		Expect(resourceReqs.Limits).To(Equal(expectedLimits))
 	})
